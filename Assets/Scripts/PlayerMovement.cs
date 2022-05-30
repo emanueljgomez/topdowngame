8abcc6ceec52ef3b070ebdfd1359ec138ca32648
Emanuel J. Gomez Sanchez <emanueljgomez@gmail.com>
2022-05-30 00:18:36 -0300

    Adds 'space marine' player character assets. Implements movement animations logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Variables
-    public float playerSpeed = 50.0f;
+    private float playerSpeed = 5.0f;
+    private Animator anim;
+    public float x, y;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
         //Player to move left, right, up, down
-        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
+        transform.Translate(Vector3.right * x * Time.deltaTime * playerSpeed);
+        transform.Translate(Vector3.forward * y * Time.deltaTime * playerSpeed);
+
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+
     }
 }
